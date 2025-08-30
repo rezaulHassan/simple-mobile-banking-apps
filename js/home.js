3ba4a91cc1aae5696783a4df4fe762d44887f963
rezaulhasan <rezaulhassan50@gmail.com>
2025-08-30 12:02:50 +0200

    home functionality done

--- js/home.js
+++ js/home.js
@@ -1,15 +1,35 @@
 // console.log('home added');
 
-// step 1 get add money btn with event listener /handler
+// step 1: get add money btn with event listener /handler
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault();
     // console.log('add money btn clicked');
 
-    // step 2 get new amount
+    // step 2: get new amount
     const addMoneyInput = document.getElementById('add-money').value;
      console.log(addMoneyInput);
 
-    //  step 3 get the pin number provided
+    //  step 3: get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+    // step 4 : verify pin number 
+    // wrong way to validate pin number 
+    if(pinNumberInput === '1234'){
+        console.log('Adding money to your account');
+        // step 4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+          console.log( typeof balance);
+        //   step 5: Add moneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step 6: Update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }  
+    else{
+        alert('failed to add money! please try again');
+    }
 })
