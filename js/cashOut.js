09a2f56fd6a34a437cdcd2a1b9db6f2e02b69167
rezaulhasan <rezaulhassan50@gmail.com>
2025-08-31 09:20:02 +0200

    features section done

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,26 +1,21 @@
 document.getElementById('btn-cash-out').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('cash out btn clicked');
+   
+    // step 2: get the Cash out amount
+    const cashOutInput = document.getElementById('input-cash-out').value;
+    const cashOutNumber = parseFloat(cashOutInput);
+    const CashOutPin = document.getElementById('input-cash-out-pin').value;
 
-    const cashOut = document.getElementById('input-cash-out').value;
-    const pinNumber = document.getElementById('input-cash-out-pin').value;
-    console.log(cashOut, pinNumber);
+    if(CashOutPin === '1234'){
+      const balance = document.getElementById('account-balance').innerText;
+      const balanceNumber = parseFloat(balance);
+      const newBalance = balanceNumber - cashOutNumber;
+      console.log(newBalance);
 
-    // wrong way to verify pin number
-    if(pinNumber === '1234'){
-        const balance = document.getElementById('account-balance').innerText;
-        const balanceNumber = parseFloat(balance);
-        const cashOutNumber = parseFloat(cashOut);
-        // reduce the balance
-        const newBalance = balanceNumber - cashOutNumber;
-
-        // update the UI
-        document.getElementById('account-balance').innerText = newBalance;
+      document.getElementById('account-balance').innerText = newBalance;
 
     }
     else{
-       alert('Failed to login ! Please try again later ')
+        alert('Wrong Pin number ! please try again later')
     }
-
-
 })
